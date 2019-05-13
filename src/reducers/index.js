708867d1d02f0b3ad0fb68e3cea29502cc7e1c11
Manju KC <Manju KC>
2019-05-13 16:12:41 -0400

    increment button is increment the counter by one

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -13,8 +13,12 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
     // Fill in the body of this case
+    return{
+      ...state, count: initialState.count++
+    }
     case DECREMENT:
     // Fill in the body of this case
+    
     default:
       return state;
   }
